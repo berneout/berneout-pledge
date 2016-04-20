b7f45b5be4a302de0b64db0e8311adc357751413
Kyle E. Mitchell <kyle@kemitchell.com>
2016-04-19 22:26:29 -0700

    Fill in pledge name and edition

--- sign-the-pledge.sh
+++ sign-the-pledge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+edition=$(git tag -l --points-at HEAD 2>/dev/null)
+
 timestamp=$(date --utc --iso-8601=minutes)
 
 read -p "Enter you GitHub user name, without @: " user
@@ -8,6 +10,10 @@ read -p "Sign your name by typing it between /slashes/: " signature
 sed --in-place -- "s!{{{GITHUB_USER_NAME}}}!$user!g" pledge
 sed --in-place -- "s!{{{SIGNATURE}}}!$signature!g" pledge
 sed --in-place -- "s!{{{TIMESTAMP}}}!$timestamp!g" pledge
+
+if [ -n "$edition" ]; then
+  sed --in-place -- "s!{{{PLEDGE}}}!The Berneout Pledge $edition!g" pledge
+fi
 
 git add pledge
 git commit -m "Customize for @$user"
