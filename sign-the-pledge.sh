4acb3be50cbf6d9f860bf4159a95c494abd0ed79
Kyle E. Mitchell <kyle@kemitchell.com>
2016-04-19 22:41:14 -0700

    Warn and exit for untagged versions of the pledge

--- sign-the-pledge.sh
+++ sign-the-pledge.sh
@@ -2,6 +2,14 @@
 
 edition=$(git tag -l --points-at HEAD 2>/dev/null)
 
+if [ -z "$edition" ]; then
+cat > /dev/stderr <<-EOS
+This is not an official edition of The Berneout Pledge!  Please visit
+https://github.com/berneout/berneout-pledge for the latest edition!
+EOS
+exit 1
+fi
+
 timestamp=$(date --utc --iso-8601=minutes)
 
 read -p "Enter you GitHub user name, without @: " user
@@ -10,10 +18,7 @@ read -p "Sign your name by typing it between /slashes/: " signature
 sed --in-place -- "s!{{{GITHUB_USER_NAME}}}!$user!g" pledge
 sed --in-place -- "s!{{{SIGNATURE}}}!$signature!g" pledge
 sed --in-place -- "s!{{{TIMESTAMP}}}!$timestamp!g" pledge
-
-if [ -n "$edition" ]; then
-  sed --in-place -- "s!{{{PLEDGE}}}!The Berneout Pledge $edition!g" pledge
-fi
+sed --in-place -- "s!{{{PLEDGE}}}!The Berneout Pledge $edition!g" pledge
 
 git add pledge
 git rm -f sign-the-pledge.sh
